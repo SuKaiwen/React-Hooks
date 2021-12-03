303290bd557ba5381175c789337cbf21466a8f8a
Kevin Su <kevinsu@outlook.co.nz>
2021-12-03 18:14:32 +1300

    useLayoutEffect

--- src/Components/LayoutEffect.js
+++ src/Components/LayoutEffect.js
@@ -1,21 +1,26 @@
-import React, {useLayoutEffect, useEffect} from 'react';
+import React, {useLayoutEffect, useEffect, useRef} from 'react';
 
 function LayoutEffect(props) {
     
     // useLayoutEffect is very similar to useEffect
     // useLayoutEffect is called before useEffect
+    const inputRef = useRef(null);
 
+    // We can use this when we want to change
+    // the layout of the UI before the data
+    // is called...
     useLayoutEffect(() => {
-
-    });
+        console.log(inputRef.current.value);
+    }, []);
 
     useEffect(() => {
-
-    });
+        inputRef.current.value = "Hello";
+    }, []);
 
     return (
         <div>
             <h1>Layout Effect</h1>
+            <input ref={inputRef} value="HI"/>
         </div>
     );
 }
